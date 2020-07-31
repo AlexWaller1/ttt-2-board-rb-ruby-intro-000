6d38306276431303266db3738e516a4c9808da9d
grateful-closure-7122 <alex@artofalexwaller.com>
2020-07-31 04:15:36 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,31 +1,6 @@
-row=["0","1","2","3","4","5","6","7","8"]# Define the variable board below.
+Tic_Tac_Toe="board"
 
-# Position 1:Top Left
-row[0]
+board=[]
 
-# Position 2:Top Center
-row[1]
+["0","1","2","3","4","5","6","7","8"]
 
-# Position 3:Top Right
-row[2]
-
-# Position 4:Middle Left
-row[3]
-
-# Position 5:Middle Center
-row[4]
-
-# Position 6: Right Center
-row[5]
-
-# Position 7: Bottom Left
-row[6]
-
-# Position 8: Bottom Center
-row[7]
-
-# Position 9: Bottom Right
-row[8]
-
-# Move X to the Position 2
-row[1]="X"
